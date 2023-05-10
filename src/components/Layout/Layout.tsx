664c6350807ccd53e28b7d2f62e3a23f340dec30
Aleksey Yesin <esinru@gmail.com>
2023-05-10 16:21:12 +0300

    Connect font

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { FC, PropsWithChildren } from "react";
+import clsx from "clsx";
 import Header from "./Header/Header";
+import { titillium } from "@/styles/fonts";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div>
+    <div className={clsx(titillium.variable, titillium.className)}>
       <Header />
       {children}
     </div>
---
+++ src/styles/fonts.ts
@@ -0,0 +1,9 @@
+import { Titillium_Web } from "next/font/google";
+
+const titillium = Titillium_Web({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-titillium",
+});
+
+export { titillium };
